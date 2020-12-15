0060c8e4c0596e0be1acabdf54ae2ccd48415654
Matias Zappino <matiaszappino31@gmail.com>
2020-12-15 23:32:00 +0000

    Ex 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_c = ()
-    appending_a = []
-    appending_b = []
+    appenda = []
+    appendb = []
 
     if len(tuple_a) >= 2 and len(tuple_b) >= 2:
         tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     else:
         if len(tuple_a) == 1:
-            appending_a.append(tuple_a[0])
-            appending_a.append(0)
+            appenda.append(tuple_a[0])
+            appenda.append(0)
         if len(tuple_b) == 1:
-            appending_b.append(tuple_b[0])
-            appending_b.append(0)
+            appendb.append(tuple_b[0])
+            appendb.append(0)
         if len(tuple_a) == 0:
-            appending_a.append(0)
-            appending_a.append(0)
+            appenda.append(0)
+            appenda.append(0)
         if len(tuple_b) == 0:
-            appending_b.append(0)
-            appending_b.append(0)
-        if len(appending_a) == 0:
-            appending_a.append(tuple_a[0])
-            appending_a.append(tuple_a[1])
-        if len(appending_b) == 0:
-            appending_b.append(tuple_b[0])
-            appending_b.append(tuple_b[1])
-        tuple_c = (appending_a[0] + appending_b[0], appending_a[1] + appending_b[1])
+            appendb.append(0)
+            appendb.append(0)
+        if len(appenda) == 0:
+            appenda.append(tuple_a[0])
+            appenda.append(tuple_a[1])
+        if len(appendb) == 0:
+            appendb.append(tuple_b[0])
+            appendb.append(tuple_b[1])
+        tuple_c = (appenda[0] + appendb[0], appenda[1] + appendb[1])
     return tuple_c
