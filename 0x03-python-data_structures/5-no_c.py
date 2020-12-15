54eaf55beacea8f585a0d9533ff9d150710c2e2f
Matias Zappino <matiaszappino31@gmail.com>
2020-12-15 20:42:54 +0000

    Ex 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    if my_string:
+        new_string = ""
+        for i in range(len(my_string)):
+            if my_string[i] != "C" and my_string[i] != "c":
+                new_string += my_string[i]
+    return new_string
