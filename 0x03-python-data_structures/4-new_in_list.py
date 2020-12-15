18cfd4f4235dc41c8153087440983180817ee349
Matias Zappino <matiaszappino31@gmail.com>
2020-12-15 19:56:11 +0000

    Ex 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,5 +5,6 @@ def new_in_list(my_list, idx, element):
     if idx >= len(my_list):
         return my_list
     else:
-        my_list[idx] = element
-        return my_list
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
