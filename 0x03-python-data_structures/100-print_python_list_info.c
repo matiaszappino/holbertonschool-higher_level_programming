30c2cdd952d5ace1561f3abda698fa4bd83824bf
Matias Zappino <matiaszappino31@gmail.com>
2020-12-16 02:32:49 +0000

    Ex 14

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,19 @@
+#include <Python.h>
+/**
+* print_python_list_info - print information about lists in python
+ * @p: list
+*/
+void print_python_list_info(PyObject *p)
+{
+    int tope, i = 0, cant = 0;
+    PyObject *lista; 
+    
+    tope = Py_SIZE(p);
+    cant = ((PyListObject *)p)->allocated;
+    printf("[*] Size of the Python List = %d\n[*] Allocated = %d\n", tope, cant);
+    for (; i <= tope - 1; i++)
+    {
+        lista = PyList_GetItem((p), (i));
+        printf("Element %d: %s\n", i, Py_TYPE(lista)->tp_name);
+    }
+}
