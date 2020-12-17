9f58108da9fd18156f032df5fbbb53d3fcebb9d9
Matias Zappino <matiaszappino31@gmail.com>
2020-12-17 19:00:20 +0000

    Ex 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dic = {}
-    for i, j in a_dictionary.items():
-        if i != key:
-            new_dic[i] = a_dictionary[i]
-        if i == key:
-            new_dic[i] = value
-        if key not in a_dictionary:
-            new_dic[key] = value
+    new_dic = a_dictionary
+    new_dic[key] = value
     return new_dic
