c6fb7663f168827b64a96df20bd3d2bde77721ee
Matias Zappino <matiaszappino31@gmail.com>
2020-12-17 21:26:57 +0000

    Ex 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    best_key = []
-    valor = list(a_dictionary.values())
-    nombre = list(a_dictionary.keys())
-    best_key.append(nombre[valor.index(max(valor))]
-    print(best_key[0])
-    return best_key[0]
+    if a_dictionary:
+        new_dic = sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True)
+        return new_dic[0][0]
+    else:
+        return None
