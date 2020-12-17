bbe0279ead7697e584dbc1f347610f8cf9704207
Matias Zappino <matiaszappino31@gmail.com>
2020-12-17 17:19:02 +0000

    Ex 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -2,9 +2,9 @@
 def only_diff_elements(set_1, set_2):
     new_set = []
         for i in set_1:
-            if i in set_2:
+            if i not in set_2:
                 new_set.append(i)
         for j in set_2:
-            if j in set_1 and j not in new_set:
+            if j not in set_1 and j not in new_set:
                 new_set.append(j)
     return new_set
