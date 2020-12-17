09131a160c1d199954aca45c59a79de1735454d7
Matias Zappino <matiaszappino31@gmail.com>
2020-12-17 19:07:58 +0000

    Ex 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if key:
-        del a_dictionary[key]
+        a_dictionary.pop(key, None)
         return a_dictionary
     else:
         return a_dictionary
