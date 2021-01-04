0bf88d1b1bab5b98f30311b6371352fb24e1a09e
Matias Zappino <matiaszappino31@gmail.com>
2021-01-04 21:23:28 +0000

    Ex 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        a = args[0]
+        b = args[1]
+        c = fct(a,b)
+    except Exception as e:
+        c = None
+        string = "Exception: " + str(e) + '\n'
+        sys.stderr.write(string)
+    return c
