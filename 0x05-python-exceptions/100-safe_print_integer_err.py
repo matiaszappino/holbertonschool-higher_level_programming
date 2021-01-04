d6df5581012c817d56e6076490308f7383fca9eb
Matias Zappino <matiaszappino31@gmail.com>
2021-01-04 20:38:37 +0000

    Ex 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
+        return True
     except Exception as e:
-        print("Exception: {}".format(e))
+        string = "Exception: " + str(e) + '\n'
+        sys.stderr.write(string)
         return False
-    return True
