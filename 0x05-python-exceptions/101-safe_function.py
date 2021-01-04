e6a9fcbbf65d55fc3ff98c8674fac08fa05e151f
Matias Zappino <matiaszappino31@gmail.com>
2021-01-04 21:26:20 +0000

    Ex 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,8 +5,8 @@ def safe_function(fct, *args):
         a = args[0]
         b = args[1]
         c = fct(a, b)
+        return c
     except Exception as e:
-        c = None
-        string = "Exception: " + str(e) + '\n'
+        string = "Exception:" + str(e) + '\n'
         sys.stderr.write(string)
-    return c
+        return None
