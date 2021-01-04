e001ef55469ff28d2eac047eff37237a9530964f
Matias Zappino <matiaszappino31@gmail.com>
2021-01-04 22:08:59 +0000

    Ex 9

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result = a ** b
+            result = result / i
+        except:
+            result = b + a
+            break
+        return result
