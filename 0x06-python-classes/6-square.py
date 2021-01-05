96c7e871d67af58643fcce594c3de4831ee3327d
Matias Zappino <matiaszappino31@gmail.com>
2021-01-05 20:29:23 +0000

    Added Documentation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,6 +8,18 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Def Init Size and Position"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if type(position) is not tuple:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif len(position) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif type(position[0]) != int or type(position[1]) != int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
         self.__size = size
         self.__position = position
 
