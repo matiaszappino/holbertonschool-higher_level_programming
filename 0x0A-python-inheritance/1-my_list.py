d524eaf5aa2c8b0ace2707d29a5f45ace708e75f
Matias Zappino <matiaszappino31@gmail.com>
2021-01-19 21:38:31 +0000

    Ex 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """Printed Sorted"""
 
+if __name__ == "__main__":
+    """Test cases"""
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
+
 
 class MyList(list):
     """Class Mylist"""
