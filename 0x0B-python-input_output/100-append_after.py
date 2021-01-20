9e2242e98296a39d0eb31c02f6494d9509670e41
Matias Zappino <matiaszappino31@gmail.com>
2021-01-20 21:31:08 +0000

    Ex 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,9 +5,9 @@
 def append_after(filename="", search_string="", new_string=""):
     """Inserts a line of text to a file"""
     inputfile = open(filename, 'r').readlines()
-    write_file = open(filename, 'w')
-    for line in inputfile:
-        write_file.write(line)
-        if search_string in line:
-            write_file.write(new_string)
-    write_file.close()
+    with open(filename, 'w') as write_file:
+        for line in inputfile:
+            write_file.write(line)
+            if search_string in line:
+                write_file.write(new_string)
+        write_file.close()
