931f5c6ffc34bbfc421ab58e728f437dde0f5d7b
Matias Zappino <matiaszappino31@gmail.com>
2021-01-20 20:16:20 +0000

    Ex 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""Student"""
+"""Module Documentation"""
 
 
 class Student:
     """Student Class"""
+
     def __init__(self, first_name, last_name, age):
-        """Init Student Class"""
-        self.first_name = first_name
-        self.last_name = last_name
+        """Constructor method"""
         self.age = age
+        self.last_name = last_name
+        self.first_name = first_name
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance"""
-        flag = 0
-        new_dict = {}
-
-        if type(attrs) is list:
-            for i in attrs:
-                if type(i) != str:
+        """Json function"""
+        if attrs is not None:
+            new_dict = {}
+            for obj in attrs:
+                if type(obj) != str:
                     return self.__dict__
-            for i in attrs:
-                if i not in new_dict and i not in self.__dict__:
-                    new_dict[i] = 
+            for obj in attrs:
+                if obj in self.__dict__:
+                    new_dict[obj] = self.__dict__[obj]
+            return new_dict
+        else:
             return self.__dict__
