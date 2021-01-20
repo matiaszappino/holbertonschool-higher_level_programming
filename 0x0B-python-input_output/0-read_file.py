b7ea9403cae682d3972dc243df99c11a515ab9eb
Matias Zappino <matiaszappino31@gmail.com>
2021-01-20 20:12:24 +0000

    Ex 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, 'r') as f:
-        print(f.read())
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
