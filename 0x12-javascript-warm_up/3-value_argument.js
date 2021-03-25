003b2443e65f5b54cfae2befde72ad7c95a401be
matiaszappino <matiaszappino31@gmail.com>
2021-03-25 02:20:45 +0000

    Semistandard and Errors Fixed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const myVar = process.argv.length;
-if (myVar === 2) {
-  console.log('No argument');
-} else {
+if (process.argv[2]) {
   console.log(process.argv[2]);
+} else {
+  console.log('No argument');
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 let x;
 let i;
-if (x = parseInt(process.argv[2])) {
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  x = parseInt(process.argv[2]);
   for (i = 0; i < x; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
