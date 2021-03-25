ddd5d7ba8716be84ab39b9ce79042ca28729f2f2
matiaszappino <matiaszappino31@gmail.com>
2021-03-25 03:04:25 +0000

    Semistandard Fixed

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const myObject = {
-    type: 'object',
-    value: 12
-  };
+  type: 'object',
+  value: 12
+};
 console.log(myObject);
 myObject.incr = () => { myObject.value += 1; };
 myObject.incr();
