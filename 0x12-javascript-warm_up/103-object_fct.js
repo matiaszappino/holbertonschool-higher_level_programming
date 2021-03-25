e6f01db00a5c8eff54383574461ddbca0e041fc9
matiaszappino <matiaszappino31@gmail.com>
2021-03-25 03:03:18 +0000

    Ex 17

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -3,13 +3,11 @@ const myObject = {
     type: 'object',
     value: 12
   };
-  console.log(myObject);
-  /*
-  YOUR CODE HERE
-  */
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
-  myObject.incr();
-  console.log(myObject);
+console.log(myObject);
+myObject.incr = () => { myObject.value += 1; };
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
