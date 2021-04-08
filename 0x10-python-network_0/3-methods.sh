e093f436b55f6c54d27f7622b02da1664254b181
Matias Zappino <matiaszappino31@gmail.com>
2021-04-08 19:26:48 +0000

    All Requirements Fixed

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s -I "$1" | grep Allow | cut -d ":" -f2
+curl -s -I "$1" | grep Allow | cut -d ":" -f 2-
