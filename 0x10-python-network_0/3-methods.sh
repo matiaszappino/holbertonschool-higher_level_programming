8ca308ff52f54f601ce630262797ffd079799ebf
Matias Zappino <matiaszappino31@gmail.com>
2021-04-08 18:06:21 +0000

    All Requirements Fixed

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL and displays all HTTP methods the server will accept.
-
 curl -sI "$1" | grep Allow | awk '{print $2" "$3" "$4}'
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -X POST -F 'email=hr@holbertonschool.com' -F 'subject=I will always be here for PLD' "$1"
+curl -sX POST -F 'email=hr@holbertonschool.com' -F 'subject=I will always be here for PLD' "$1"
