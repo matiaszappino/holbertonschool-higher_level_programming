95e2a47746e82804b30d7885c698aa15a40da0c0
Matias Zappino <matiaszappino31@gmail.com>
2021-04-08 20:12:40 +0000

    Ex 6 Finished

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function that finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    array_len = len(list_of_integers)
+    if (array_len == 1):
+        return array_len[0]
+    elif (array_len == 2):
+        if (list_of_integers[1] >= list_of_integers[0]):
+            return list_of_integers[1]
+    else:
+        for elem in range(len(list_of_integers) - 1):
+            if list_of_integers[elem] >= list_of_integers[elem + 1] and list_of_integers[elem] >= list_of_integers[elem - 1]:
+                return list_of_integers[elem]
