f9c00ded6be0cc8351ff0e89f7755e0c2cac33a3
Matias Zappino <matiaszappino31@gmail.com>
2021-04-10 01:47:11 +0000

    All Mandatory Ex's Finished

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Python script that takes 2 arguments in order 
+to solve this challenge.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL and an email, sends a POST 
+request to the passed URL with the email as a parameter, 
+and displays the body of the response (decoded in utf-8) """
+
+
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+        url = sys.argv[1]
+        values = {"email": sys.argv[2]}
+        data = urllib.parse.urlencode(values)
+        data = data.encode("ascii")
+        req = urllib.request.Request(url, data)
+        with urllib.request.urlopen(req) as response:
+                html = response.read()
+                print(html.decode("utf-8"))
